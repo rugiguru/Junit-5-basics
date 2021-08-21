e3e61e68ac6f901177117cef263ee6b77f5fbc8b
rugiguru <31921091+rugiguru@users.noreply.github.com>
2021-08-21 16:53:49 +0530

    junit life cycle

--- src/main/java/com/coderlogs/MathUtils.java
+++ src/main/java/com/coderlogs/MathUtils.java
@@ -7,6 +7,11 @@ public class MathUtils {
 		return a +b;
 	}
 	
+	public int divide(int a, int b)
+	{
+		return a/b;
+	}
+	
 	public double circleArea(double radius)
 	{
 		return Math.PI * radius * radius;
--- src/test/java/com/coderlogs/MathUtilsTest.java
+++ src/test/java/com/coderlogs/MathUtilsTest.java
@@ -2,24 +2,36 @@ package com.coderlogs;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
+	
+	MathUtils maths;
+	
+	@BeforeEach
+	void init() {
+		maths = new MathUtils();
+	}
+	
 
 	@Test
 	void testAdd() {
-		
 		int expected = 2;
-		MathUtils mthu = new MathUtils();
-		int actual =  mthu.add(1, 1);
+		int actual =  maths.add(1, 1);
 		assertEquals(expected, actual,"the method should add two numbers");
 	}
 	
 	@Test
 	void testCrcleradiues()
 	{
-		MathUtils maths = new MathUtils();
 		assertEquals(314.1592653589793, maths.circleArea(10), " Should return the area of circle");
+	}
+	
+	@Test
+	void testDivide()
+	{
+		assertThrows(ArithmeticException.class, () -> maths.divide(1, 0), "Divide by zero should throw ex");
 	}
 
 }
