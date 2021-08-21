d845c43e88930b77ee038a7b9d65631e8cfc5b0e
rugiguru <31921091+rugiguru@users.noreply.github.com>
2021-08-21 16:13:03 +0530

    Basics of junit

---
+++ src/main/java/com/coderlogs/MathUtils.java
@@ -0,0 +1,15 @@
+package com.coderlogs;
+
+public class MathUtils {
+	
+	public int add(int a, int b)
+	{
+		return a +b;
+	}
+	
+	public double circleArea(double radius)
+	{
+		return Math.PI * radius * radius;
+	}
+
+}
