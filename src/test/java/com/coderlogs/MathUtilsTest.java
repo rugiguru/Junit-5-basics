2a5a59c794685b253eb8ecb102ccf860d8e660af
rugiguru <31921091+rugiguru@users.noreply.github.com>
2021-08-25 17:27:28 +0530

    assume true annotations

--- src/test/java/com/coderlogs/MathUtilsTest.java
+++ src/test/java/com/coderlogs/MathUtilsTest.java
@@ -1,19 +1,46 @@
 package com.coderlogs;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
 	
 	MathUtils maths;
+	
+//	@BeforeAll
+//	static void beforeAllInit()
+//	{
+//		System.out.println("This needs to run before all");
+//	}
+	
+//	@EnabledOnOs
+//	@EnabledOnJre
+//	@EnabledIfEnvironmentVariable
 	
 	@BeforeEach
 	void init() {
 		maths = new MathUtils();
 	}
 	
+	
+	@Test
+	@DisplayName("This TDD is always disabled")
+	@Disabled
+	void disabledtest()
+	{
+		fail("Alwasy fails");
+	}
 
 	@Test
 	void testAdd() {
@@ -23,8 +50,11 @@ class MathUtilsTest {
 	}
 	
 	@Test
+	@DisplayName("Test for area of circle")
 	void testCrcleradiues()
 	{
+//		boolean isServerUp = false;
+//		assumeTrue(isServerUp);
 		assertEquals(314.1592653589793, maths.circleArea(10), " Should return the area of circle");
 	}
 	
