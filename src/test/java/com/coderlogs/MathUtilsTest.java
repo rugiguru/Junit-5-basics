443e79c2a57ae23904c7eeb02e1e18120fea0e69
rugiguru <31921091+rugiguru@users.noreply.github.com>
2021-08-25 18:04:06 +0530

    added test reporting

--- src/test/java/com/coderlogs/MathUtilsTest.java
+++ src/test/java/com/coderlogs/MathUtilsTest.java
@@ -7,8 +7,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.condition.EnabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnOs;
@@ -17,7 +20,8 @@ import org.junit.jupiter.api.condition.EnabledOnOs;
 class MathUtilsTest {
 	
 	MathUtils maths;
-	
+	TestInfo testInfo;
+	TestReporter testReported;
 //	@BeforeAll
 //	static void beforeAllInit()
 //	{
@@ -29,10 +33,12 @@ class MathUtilsTest {
 //	@EnabledIfEnvironmentVariable
 	
 	@BeforeEach
-	void init() {
+	void init(TestInfo testInfo, TestReporter testReported) {
+		this.testInfo = testInfo;
+		this.testReported = testReported;
 		maths = new MathUtils();
+		testReported.publishEntry("Running " + testInfo.getDisplayName() + " with tags " + testInfo.getTags());
 	}
-	
 	
 	@Test
 	@DisplayName("This TDD is always disabled")
@@ -43,22 +49,28 @@ class MathUtilsTest {
 	}
 
 	@Test
+	@Tag("Math")
 	void testAdd() {
 		int expected = 2;
 		int actual =  maths.add(1, 1);
 		assertEquals(expected, actual,"the method should add two numbers");
+		
 	}
 	
 	@Test
 	@DisplayName("Test for area of circle")
+	@Tag("Area")
 	void testCrcleradiues()
 	{
 //		boolean isServerUp = false;
 //		assumeTrue(isServerUp);
+//		System.out.println();
 		assertEquals(314.1592653589793, maths.circleArea(10), " Should return the area of circle");
 	}
 	
+
 	@Test
+	@Tag("Math")
 	void testDivide()
 	{
 		assertThrows(ArithmeticException.class, () -> maths.divide(1, 0), "Divide by zero should throw ex");
